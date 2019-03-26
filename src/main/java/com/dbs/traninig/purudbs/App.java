eba6930af512ec7dd5581156a438d1aa8b8f1cf7
Puru9848 <purushottambitpatna@gmail.com>
2019-03-26 04:41:58 -0700

    First project in maven

---
+++ src/main/java/com/dbs/traninig/purudbs/App.java
@@ -0,0 +1,15 @@
+package com.dbs.traninig.purudbs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      Calculator cl = new Calculator();
+      cl.add(5, 5);
+      cl.sub(10, 5)
+    }
+}
