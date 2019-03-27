9984f84fef02bfd0d5038f3a2bd4f7c263f8d7aa
Purushottam Kumar <purushottambitpatna@gmail.com>
2019-03-27 10:08:37 +0530

    Update Test.java

--- src/test/java/com/dbs/traninig/purudbs/Test.java
+++ src/test/java/com/dbs/traninig/purudbs/Test.java
@@ -1,5 +1,14 @@
 package com.dbs.traninig.purudbs;
 
 public class Test {
+  
+  
+	@Test
+	public void test() {
+		App app = new App();
+		String msg = app.greet();
+		assertEquals("Hello", msg);
+	}
+
 
 }
